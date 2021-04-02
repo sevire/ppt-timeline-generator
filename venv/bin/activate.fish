6a84afba7bf8bd399c99312c8401a295adcbc2d7
Thomas (LI) <tbg-livestockinformation@genonline.co.uk>
2021-04-02 13:34:04 +0100

    Remove venv from git - should set up for each dev environment

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/thomasgaylardou/PycharmProjects/ppt-timeline3/venv"
+set -gx VIRTUAL_ENV "/Users/livestockinformation/PycharmProjects/ppt-timeline-generator/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
